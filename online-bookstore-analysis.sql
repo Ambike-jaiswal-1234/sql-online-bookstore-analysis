f6c5db4cadab3bd9649533f7c1d2b0092edea08e
Ambike Jaiswal <thyselfseeking@gmail.com>
2026-05-09 06:59:49 +0530

    Refactor SQL script for online bookstore analysisAdded SQL bookstore analysis project with business queries and documentation

--- online-bookstore-analysis.sql
+++ online-bookstore-analysis.sql
@@ -74,35 +74,35 @@ WHERE country='Canada';
 SELECT * FROM Orders
 WHERE order_date BETWEEN '2023-11-01' AND '2023-11-30';
 
--- 5) Retrieve the total stock of books available:
+-- BUSINESS QUERY 5: Retrieve the total stock of books available
 SELECT SUM(stock) AS tOTAL_Stock
 FROM books;
 
--- 6) Find the details of the most expensive book:
+-- BUSINESS QUERY 6: Find the details of the most expensive book
 SELECT * FROM books
 ORDER BY Price DESC 
 LIMIT 1;
 
--- 7) Show all customers who ordered more than 1 quantity of a book:
+-- BUSINESS QUERY 7: Show all customers who ordered more than 1 quantity of a book
 SELECT * FROM Orders
 WHERE quantity>1;
 
--- 8) Retrieve all orders where the total amount exceeds $20:
+-- BUSINESS QUERY 8: Retrieve all orders where the total amount exceeds $20
 SELECT * FROM Orders
 WHERE total_amount>20;
 
---9) List all genres available in the Books table:
+-- BUSINESS QUERY 9: List all genres available in the Books table
 SELECT DISTINCT * FROM Books;
 
---10) Find the book with the lowest stock:
+-- BUSINESS QUERY 10: Find the book with the lowest stock
 SELECT * FROM books ORDER BY stock LIMIT 1;
 
--- 11) Calculate the total revenue generated from all orders:
+-- BUSINESS QUERY 11: Calculate the total revenue generated from all orders
 SELECT SUM(total_amount) AS Revenue FROM Orders;
 
 -- ADVANCE BUSINESS ANALYSIS QUERIES:
 
---1) Retrieve the total number of books sold for each genre:
+-- BUSINESS QUERY 1: Retrieve the total number of books sold for each genre
 SELECT * FROM Orders;
 
 SELECT b.Genre, SUM(o.quantity) AS Total_books_Sold
@@ -114,11 +114,11 @@ ON
 o.book_id = b.book_id
 GROUP BY b.Genre;
 
--- 2) Find the average price of books in the "fantasy" genre:
+-- BUSINESS QUERY 2: Find the average price of books in the "fantasy" genre
 SELECT AVG(price) AS Average_Price FROM Books
 WHERE Genre = 'Fantasy';
 
--- 3) List customers who have placed at least 2 orders:
+-- BUSINESS QUERY 3: List customers who have placed at least 2 orders
 SELECT o.customer_id, c.name, COUNT(o.Order_id) AS ORDER_COUNT
 FROM orders o
 JOIN customers c ON o.customer_id=c.customer_id
@@ -127,19 +127,19 @@ GROUP BY o.customer_id, c.name
 
 HAVING COUNT(Order_id) >=2; 
 
--- 4) Find the most frequently ordered book:
+-- BUSINESS QUERY 4: Find the most frequently ordered book
 SELECT o. Book_id, b.title, COUNT(o.order_id) AS ORDER_COUNT
 FROM orders o
 JOIN books b ON o.book_id=b.book_id
 GROUP BY o.book_id, b.title
 ORDER BY ORDER_COUNT DESC LIMIT 1; 
 
--- 5) Show the top 3 most expensive books of 'fantasy' Genre:
+-- BUSINESS QUERY 5: Show the top 3 most expensive books of 'fantasy' Genre
 SELECT * FROM books
 WHERE genre ='Fantasy'
 ORDER BY price DESC LIMIT 3;
 
--- 6) Retrieve the tolat quantity of books sold by each author:
+-- BUSINESS QUERY 6: Retrieve the tolat quantity of books sold by each author
 SELECT b.author, SUM(o.quantity) AS Total_books_sold
 FROM 
 orders o
@@ -148,7 +148,7 @@ books b
 ON o.book_id=b.book_id
 GROUP BY b.Author; 
 
--- 7) List the cities where customers who spent over $30 are located:
+-- BUSINESS QUERY 7: List the cities where customers who spent over $30 are located
 SELECT DISTINCT c.city , total_amount
 FROM
 orders o
@@ -157,7 +157,7 @@ customers c
 ON o.customer_id=c.customer_id
 WHERE o.total_amount > 30;
 
--- 8) Find the customer who spent the most on orders:
+-- BUSINESS QUERY 8: Find the customer who spent the most on orders
 SELECT c.customer_id, c.name, SUM(O.total_amount) AS Total_Spent
 FROM 
 orders o
@@ -167,7 +167,7 @@ ON o.customer_id=c.customer_id
 GROUP BY c.customer_id, c.name
 ORDER BY Total_spent Desc;
 
--- 9) Calculate bthe stock remaining after fulfilling all orders:
+-- BUSINESS QUERY 9: Calculate bthe stock remaining after fulfilling all orders
 SELECT b.book_id, b.title, b.stock, COALESCE(SUM(o.quantity),0) AS Order_quantity,
        b.stock- COALESCE(SUM(o.quantity),0) AS Remaining_Quantity
 FROM 
